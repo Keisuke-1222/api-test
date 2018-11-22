dd9d0f5bcd0888553caeca8589f5f21d61b3efeb
keisuke-1222 <s.k.s.k.ra20@gmail.com>
2018-11-22 17:31:07 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   root to: 'addresses#search'
-  get 'addresses', to: 'addresses#search'
-  resources :addresses, only: [:index, :show]
+  resources :addresses, only: :show
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
